32f8a40094a6328602fb20db734cba2c2645577f
Gatsby <111303029+mattjgatsby@users.noreply.github.com>
2022-09-28 21:57:49 -0400

    wrote out function for password length

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+var passLength 
 // Write password to the #password input
 function writePassword() {
+  grabPassLength();
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  
+
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function grabPassLength (){
+  passLength = prompt("Choose number between 8 and 128.")
+
+if(passLength >= 8 && passLength <= 128) {
+
+}else(grabPassLength()); 
+
+
+}
