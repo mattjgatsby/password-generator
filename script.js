2dd151d3860c2791edb830e5a20da2c289c2df02
Gatsby <111303029+mattjgatsby@users.noreply.github.com>
2022-09-29 01:05:49 -0400

    added calls for functions within writepassword function

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var lowLetter = true;
 var passLength 
-var lowerLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u", "v", "w", "x","y", "z"]
-var upperLetters = ["A","B", "C", "D", "E","F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var lowerLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u", "v", "w", "x","y", "z"];
+var upperLetters = ["A","B", "C", "D", "E","F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var specialChar = [
   '[', '`', '!', '@',  '#', '$', '%',
   '^', '&', '*', '(',  ')', '_', '+',
   '-', '=', '[', ']',  '{', '}', ';',
   "'", ':', '"', '\\', '|', ',', '.',
   '<', '>', '/', '?',  '~', ']', '/'
-] 
-var numbers = [0,1,2,3,4,5,6,7,8,9]
+]; 
+var numbers = [0,1,2,3,4,5,6,7,8,9];
 
 // Write password to the #password input
 function writePassword() {
   grabPassLength();
+  grabLowerLetter();
+  grabUpperLetters();
+  grabSpecialChar();
+  grabNumbers();
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   
@@ -32,6 +37,17 @@ function grabPassLength (){
 if(passLength >= 8 && passLength <= 128) {
 
 }else(grabPassLength()); 
-
-
 }
+function grabLowerLetter(){
+   lowLetter =confirm("Do you want to include lowercase letters?");
+  }
+function grabUpperLetters(){
+    confirm("Do you want to include Uppercase letters?");
+}
+function grabSpecialChar(){
+    confirm("Do you want to include Special Characters?");
+}
+function grabNumbers(){
+    confirm("Do you want to include numbers?");
+}
+function genPass()
